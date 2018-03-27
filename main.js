d9efcf5fc092e974c908eb76f925c2e9a39e1161
Tom Dugan <thomas@aadwebb.com>
2018-03-27 15:55:09 -0500

    made cards separate, added buttons

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log();
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = fancyArray => {
+    let domString = "";
+    fancyArray.forEach((animal) => {
+        domString += `<div class="card">`;
+        domString += `<img src="${animal.imageUrl}">`;
+        domString += `<h2>${animal.name}</h2>`;
+        domString += `<p>${animal.number}</p>`;
+        domString += `<button>Escaped</button>`;
+        domString += `</div>`;
+    });
+    printToDom(domString, 'card-holder');
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+    myRequest.open("GET", "animals.json");
+    myRequest.send();
+}
+
+function executeThisCodeIfXHRFails () {
+    console.log("something broke")
+}
+
+function executeThisCodeAfterFileLoaded () {
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.animals);
+}
+
+startApplication();
